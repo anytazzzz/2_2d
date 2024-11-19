8d01440f1c7096f046e52a00bed8d3b71ebc2e39
anytazzzz <igorcernienko30@gmail.com>
2024-11-19 23:56:47 +0300

    Add files via upload

--- 2_2d.py
+++ 2_2d.py
@@ -1,10 +1,10 @@
-#Условная конструкция. Операторы if, elif, else
-first=input("Ведите целое число:" )
-second=input("Еще одно:" )
-third=input("Ведите последнее целое число:" )
-if first==second and second==third:
+# Условная конструкция. Операторы if, elif, else
+first = int(input("Ведите целое число:"))
+second = int(input("Еще одно:"))
+third = int(input("Ведите последнее целое число:"))
+if first == second and second == third:
     print("Вы ввели 3 одинаковых числа")
-elif first==second or second==third or first==third:
+elif first == second or second == third or first == third:
     print("Вы ввели 2 одинаковых числа")
 else:
     print("Число совпадений Ваших чисел равно 0")
